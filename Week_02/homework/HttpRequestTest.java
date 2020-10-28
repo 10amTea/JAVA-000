1b723f89db2da9fdc8166b56e3ce9dea82ebd313
sunhz <sunhz312@163.com>
2020-10-28 23:27:07 +0800

    [DTS/AR]: N/A [Description]: week02 20201021-28 homework [IssueLink]: N/A

---
+++ Week_02/homework/HttpRequestTest.java
@@ -0,0 +1,40 @@
+package java0.nio01.homework;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class HttpRequestTest {
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final String URL = "http://localhost:8088/api/hello";
+
+    OkHttpClient client = new OkHttpClient();
+
+    public String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String result = new HttpRequestTest().get(URL);
+        System.out.println(result);
+    }
+}
